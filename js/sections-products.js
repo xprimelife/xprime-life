096d5fbf3bc824298a37b50b07e0498866f3567f
xprimelife <94486929+xprimelife@users.noreply.github.com>
2021-12-25 07:39:10 -0300

    Update sections-products.js

--- js/sections-products.js
+++ js/sections-products.js
@@ -44,6 +44,7 @@ function handleGenerateSectionProductPage(is, ip) {
           }
       
           body {
+            height: 92vh;
             font-family: Arial, Helvetica, sans-serif;
             background-color: #240029;
             color: #f2f2f2;
